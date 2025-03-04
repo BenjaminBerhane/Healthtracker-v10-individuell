3306d062febb0978ef26f1ea97a579132cea7665
rebeccalindman <rebeccalindman@icloud.com>
2025-03-04 17:54:43 +0100

    mealdata for one day

---
+++ src/utils/mockData.js
@@ -0,0 +1,43 @@
+const mealData = [
+    {
+        "name": "Bacon & Eggs",
+        "mealType": "Breakfast",
+        "id": 1,
+        "date": "2025-03-20",
+        "energy": 400,
+        "protein": 20,
+        "fat": 10,
+        "carbs": 30,
+    },
+    {   "name": "Pasta with Meatballs",
+        "mealType": "Lunch",
+        "id": 2,
+        "date": "2025-03-20",
+        "energy": 600,
+        "protein": 20,
+        "fat": 10,
+        "carbs": 30,
+    },
+    {
+        "name": "Thai Curry & Rice",
+        "mealType": "Dinner",
+        "id": 3,
+        "date": "2025-03-20",
+        "energy": 800,
+        "protein": 20,
+        "fat": 15,
+        "carbs": 40,
+    },
+    {
+        "name": "Cashews",
+        "mealType": "Snack",
+        "id": 4,
+        "date": "2025-03-20",
+        "energy": 200,
+        "protein": 10,
+        "fat": 15,
+        "carbs": 15,
+    }
+]
+
+export default mealData
