1df75b24d78bb918511185e34acc4acf5178ef3f
rebeccalindman <rebeccalindman@icloud.com>
2025-03-05 16:33:57 +0100

    installed shadcn and tailwind and adapted it to use JS

--- src/components/WeightList.jsx
+++ src/components/WeightList.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Button} from './ui/button'
 
 function WeightList() {
   return (
-    <div className=''>WeightList</div>
+    <>
+        <div className=''>WeightList</div>
+        <Button>Hej </Button>
+    </>
   )
 }
 
