7615553f7a3dc48a9550426256c845c6ebd29a8e
BenjaminBerhane <benjamin.berhane@chasacademy.se>
2025-03-11 00:43:21 +0100

    löste navlink problemet och fixat header och footer

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
-
 const Logo = () => {
   return (
-    <div  className="logo h-40 w-40">
-    <img  src="src\assets\Icon.webp" alt="Healthtracker logo" />
-    </div>
-  )
+    <figure className="max-h-[70px] max-w-[70px] md:max-h-[100px] md:max-w-[100px]">
+      <img 
+        className="h-full w-full object-contain" 
+        src="src/assets/Icon.webp" 
+        alt="Healthtracker logo" 
+      />
+    </figure>
+  );
 }
-export default Logo
+
+export default Logo;
