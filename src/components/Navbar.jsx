dd9d9c59ed1afa54325c6a567ae47d719f85bb0a
rebeccalindman <rebeccalindman@icloud.com>
2025-03-12 01:25:35 +0100

    moved styling to li item on header, added primary and secondary colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
+import { LayoutDashboard, User, Notebook } from 'lucide-react';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,7 +15,7 @@ function Navbar() {
       {/* Döljer knappen på större skärmar */}
       <button
         onClick={toggleNavbar}
-        className="px-4 py-2 hover:bg-gray-200 rounded-md md:hidden"
+        className="px-4 py-2 h-fit self-center hover:bg-accent rounded-md md:hidden"
         aria-expanded={isOpen}
         aria-label="Toggle navigation"
       >
@@ -23,17 +24,17 @@ function Navbar() {
 
       {/* Desktop-menyn visas alltid på större skärmar */}
       <ul className="hidden md:flex gap-8">
-        <li><NavLink to="/" className="px-4 py-2 hover:bg-gray-200 rounded-md">Dashboard</NavLink></li>
-        <li><NavLink to="/profilecard" className="px-4 py-2 hover:bg-gray-200 rounded-md">Profile</NavLink></li>
-        <li><NavLink to="/mealLog" className="px-4 py-2 hover:bg-gray-200 rounded-md">Meallog</NavLink></li>
+        <li className="px-4 py-2 hover:bg-accent rounded-md"><NavLink to="/" ><LayoutDashboard className="inline self-center h-full mr-2"/>Dashboard</NavLink></li>
+        <li className="px-4 py-2 hover:bg-accent rounded-md"><NavLink to="/profilecard" ><User className="inline self-center h-full mr-2"/>Profile</NavLink></li>
+        <li className="px-4 py-2 hover:bg-accent rounded-md"><NavLink to="/mealLog" ><Notebook className="inline self-center h-full mr-2"/>Meallog</NavLink></li>
       </ul>
 
       {/* Dropdown-menyn syns bara i mobilvy när knappen trycks */}
       {isOpen && (
         <ul className="md:hidden flex flex-col mt-4">
-          <li className="my-2"><NavLink to="/" className="px-4 py-2 hover:bg-gray-200 rounded-md">Dashboard</NavLink></li>
-          <li className="my-2"><NavLink to="/profilecard" className="px-4 py-2 hover:bg-gray-200 rounded-md">Profile</NavLink></li>
-          <li className="my-2"><NavLink to="/mealLog" className="px-4 py-2 hover:bg-gray-200 rounded-md">Meallog</NavLink></li>
+          <li className="my-2"><NavLink to="/" className="px-4 py-2 hover:bg-accent rounded-md">Dashboard</NavLink></li>
+          <li className="my-2"><NavLink to="/profilecard" className="px-4 py-2 hover:bg-accent rounded-md">Profile</NavLink></li>
+          <li className="my-2"><NavLink to="/mealLog" className="px-4 py-2 hover:bg-accent rounded-md">Meallog</NavLink></li>
         </ul>
       )}
     </nav>
