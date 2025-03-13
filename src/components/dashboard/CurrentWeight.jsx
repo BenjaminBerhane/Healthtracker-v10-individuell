a6c41d1cbbfcdbba852b81df98f1782f72ad7179
rebeccalindman <rebeccalindman@icloud.com>
2025-03-13 19:03:38 +0100

    svenska för current weight

--- src/components/dashboard/CurrentWeight.jsx
+++ src/components/dashboard/CurrentWeight.jsx
@@ -11,7 +11,7 @@ const CurrentWeight = () => {
       {latestWeight ? (
         <p className='text-xl w-[150px]'>
           <span className='font-bold text-xl text-primary transition duration-200'>{latestWeight.weight} </span> kg
-          <span className='block text-sm text-gray-500'> as of {latestWeight.date}</span>
+          <span className='block text-sm text-gray-500'> den {latestWeight.date}</span>
           <span className='m-2 hidden group-hover:block text-sm text-primary font-bold'> Lägg till ny vikt</span>
         </p>
       ) : (
