22a9f3ea3fec67e851345d5841ac796e9c1c97e7
rebeccalindman <rebeccalindman@icloud.com>
2025-03-14 12:39:49 +0100

    padding for MealListItem details

--- src/components/MealListItem.jsx
+++ src/components/MealListItem.jsx
@@ -9,11 +9,11 @@ const MealListItem = ({ meal, onEdit = () => {}, editButton = null }) => {
   };
 
   return (
-    <details className="bg-white shadow-md rounded p-4 mb-4 border-1 border-accent/30" onClick={toggleDetails}>
+    <details className="bg-white shadow-md rounded p-4 border-1 border-accent/30" onClick={toggleDetails}>
       <summary className="flex justify-between items-center cursor-pointer">
         <div>
-          <h3 className="text-xl font-bold mb-2 text-accent text-left">{meal.title}</h3>
-          <p className="text-gray-600">Energi: <span className="text-primary font-bold">{meal.energy}</span> kcal</p>
+          <h3 className="text-xl font-bold text-accent text-left p-2">{meal.title}</h3>
+          <p className="text-gray-600 p-2">Energi: <span className="text-primary font-bold">{meal.energy}</span> kcal</p>
         </div>
         <span className="inline-block" style={{transform: `rotate(${showDetails ? 180 : 0}deg)`}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} style={{ transform: 'rotate(90deg)' }}>
@@ -23,11 +23,11 @@ const MealListItem = ({ meal, onEdit = () => {}, editButton = null }) => {
       </summary>
       {showDetails && (
         <div className="flex flex-col items-start text-left">
-          <p className={showDetails ? 'bg-secondary w-full' : ''}>Datum: <span className="text-accent font-light">{meal.date}</span></p>
-          <p className="text-gray-600">Protein: <span className="text-primary font-bold">{meal.protein}</span> g</p>
-          <p className={showDetails ? 'bg-secondary w-full' : ''}>Kolhydrat: <span className="text-primary font-bold">{meal.carbohydrate}</span> g</p>
-          <p className="text-gray-600">Fett: <span className="text-primary font-bold">{meal.fat}</span> g</p>
-          <p className={showDetails ? 'bg-secondary w-full' : ''}>Kategori: <span className="text-accent text-light">{meal.category}</span></p>
+          <p className={showDetails ? 'bg-secondary w-full p-2' : ''}>Datum: <span className="text-accent font-light">{meal.date}</span></p>
+          <p className="text-gray-600 p-2">Protein: <span className="text-primary font-bold">{meal.protein}</span> g</p>
+          <p className={showDetails ? 'bg-secondary w-full p-2' : ''}>Kolhydrat: <span className="text-primary font-bold">{meal.carbohydrate}</span> g</p>
+          <p className="text-gray-600 p-2">Fett: <span className="text-primary font-bold">{meal.fat}</span> g</p>
+          <p className={showDetails ? 'bg-secondary w-full p-2' : ''}>Kategori: <span className="text-accent text-light">{meal.category}</span></p>
         </div>
       )}
       { editButton &&
