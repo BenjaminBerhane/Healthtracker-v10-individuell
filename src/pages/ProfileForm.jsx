42e54eb6efd3c1af80fc2fac8f64209e0d2ffb0c
rebeccalindman <rebeccalindman@icloud.com>
2025-03-14 11:57:33 +0100

    same background on all pages

--- src/pages/ProfileForm.jsx
+++ src/pages/ProfileForm.jsx
@@ -118,7 +118,7 @@ const ProfileForm = () => {
   };
 
   return (
-    <main className="flex flex-col items-center p-4 bg-green-50 gap-4">
+    <main className="flex flex-col items-center p-4 gap-4">
       <form className="w-full bg-white p-6 rounded-lg shadow-md mt-4 fixed-width">
         <h2 className="text-2xl font-bold text-center text-green-600 mb-4">
           Profil
