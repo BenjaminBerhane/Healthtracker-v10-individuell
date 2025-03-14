41cdd7e1ed59583800bba9248a5756dd3bfa8dd0
gurramannen <gustav.thilander@gmail.com>
2025-03-14 12:16:35 +0100

    separerat loadstate och savestate

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./reducers/profileSlice";
 import mealReducer from "./reducers/mealSlice";
-import { loadState, saveState } from "./utils/storage";
+import { loadMealLogs, saveMealLogs, loadProfile, saveProfile } from "./utils/storage";
 
 // Ladda data från localStorage
-const preloadedState = loadState();
+const preloadedState = {
+  meals: { mealLogs: loadMealLogs() },
+  profile: loadProfile(),
+};
 
 const store = configureStore({
   reducer: {
@@ -16,7 +19,9 @@ const store = configureStore({
 
 // Spara till localStorage vid ändringar
 store.subscribe(() => {
-  saveState(store.getState());
+  const state = store.getState();
+  saveMealLogs(state.meals.mealLogs);
+  saveProfile(state.profile);
 });
 
 export default store;
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -44,3 +44,61 @@ export const saveState = (state) => {
     console.error("Error saving state:", err);
   }
 };
+
+// Ladda mealLogs från localStorage
+export const loadMealLogs = () => {
+  try {
+    const mealLogs = JSON.parse(localStorage.getItem("mealLogs")) || [];
+    return mealLogs;
+  } catch (err) {
+    console.error("Error loading mealLogs:", err);
+    return [];
+  }
+};
+
+// Spara mealLogs till localStorage
+export const saveMealLogs = (mealLogs) => {
+  try {
+    localStorage.setItem("mealLogs", JSON.stringify(mealLogs));
+  } catch (err) {
+    console.error("Error saving mealLogs:", err);
+  }
+};
+
+// Ladda profile från localStorage
+export const loadProfile = () => {
+  try {
+    const profile = JSON.parse(localStorage.getItem("profile")) || {
+      gender: "male",
+      weight: [/* { id: 1, weight: 0, date: "åååå-mm-dd" } */],
+      height: 0,
+      age: 0,
+      activityLevel: 1.2,
+      goal: 0,
+      tdee: 0,
+      birthDate: "",
+    };
+    return profile;
+  } catch (err) {
+    console.error("Error loading profile:", err);
+    return {
+      gender: "",
+      weight: [{ id: 1, weight: 0, date: "åååå-mm-dd" }],
+      height: 0,
+      age: 0,
+      activityLevel: 0,
+      goal: 0,
+      tdee: 0,
+      birthDate: "",
+    };
+  }
+};
+
+// Spara profile till localStorage
+export const saveProfile = (profile) => {
+  try {
+    localStorage.setItem("profile", JSON.stringify(profile));
+  } catch (err) {
+    console.error("Error saving profile:", err);
+  }
+};
