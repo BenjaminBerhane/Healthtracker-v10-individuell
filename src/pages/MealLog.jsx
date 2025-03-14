283ed354bad20f4c37aaa5ed5fd3d6013ca17001
rebeccalindman <rebeccalindman@icloud.com>
2025-03-14 11:56:30 +0100

    loading take up full screen height

--- src/pages/MealLog.jsx
+++ src/pages/MealLog.jsx
@@ -63,7 +63,7 @@ const MealLog = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 flex justify-center flex-row gap-4 items-start flex-wrap">
+    <div className="container mx-auto p-4 flex justify-center flex-row gap-4 items-start flex-wrap max-w-[1000px]">
       <form onSubmit={handleSubmit} className="bg-white shadow-md card px-8 pt-6 max-w-[500px]">
         {error && <p className="text-red-500 text-xs italic mb-4">{error}</p>}
         <div className="mb-4">
@@ -90,7 +90,7 @@ const MealLog = () => {
           <label htmlFor="fat" className="block text-accent text-sm font-bold mb-2">Fett:</label>
           <input type="text" id="fat" name="fat" value={meal.fat} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-accent leading-tight focus:outline-none focus:shadow-outline" />
         </div>
-        <div className="mb-4">
+        <div className="w-fit">
           <label htmlFor="category" className="block text-accent text-sm font-bold mb-2">Kategori:</label>
           <div className="relative w-full">
             <select
